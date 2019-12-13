44481fc4412024313b742f44397bb28b2e61bfd7
chrisrusso <chris.russo99@gmail.com>
2019-12-13 04:26:03 -0300

    push, new index

--- src/js/components/nav_main.js
+++ src/js/components/nav_main.js
@@ -9,19 +9,12 @@ jQuery(document).ready(function($) {
   var actualPage = $('body').attr('id');
 
   $('.c-nav a').removeClass('f-active');
-  
-  if (actualPage == 'page-home') {
-    $('[href="index.html"]').addClass('f-active');
-  }
-  else if (actualPage == 'page-services') {
-    $('[href="services.html"]').addClass('f-active');
-  }
-  else if (actualPage == 'page-industries') {
-    $('[href="industries.html"]').addClass('f-active');
-  }
-  else if (actualPage == 'page-plans') {
-    $('[href="plans.html"]').addClass('f-active');
-  }  
+
+  if (actualPage == 'page-home') { $('[href="index.html"]').addClass('f-active'); }
+  else if (actualPage == 'page-services') { $('[href="services.html"]').addClass('f-active'); }
+  else if (actualPage == 'page-industries') { $('[href="industries.html"]').addClass('f-active'); }
+  else if (actualPage == 'page-plans') { $('[href="plans.html"]').addClass('f-active'); }
+  else if (actualPage == 'page-purchase-user') { $('[href="purchase_user.html"]').addClass('f-active'); }  
 
 
 });
